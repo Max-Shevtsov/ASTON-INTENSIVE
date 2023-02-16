117b9753ea36dac6b80560307fc97f1efb2caff2
Max <maxschew@yandex.ru>
2023-02-16 16:11:02 +0200

    add FrameLayout in MainActivity

--- Shevtcov_Lesson_3_1/app/src/main/java/com/example/flags/MainActivity.kt
+++ Shevtcov_Lesson_3_1/app/src/main/java/com/example/flags/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.flags
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageButton = findViewById<ImageView>(R.id.on_Linear_activity)
+        imageButton.setOnClickListener() {
+            val switchIntent = Intent(this, LinearLayoutActivity::class.java)
+            startActivity(switchIntent)
+        }
     }
 }
